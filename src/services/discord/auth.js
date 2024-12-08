b4e0a814c25f0fec0964dea94033b4ffec649e00
SyntroXofficial <162024542+SyntroXofficial@users.noreply.github.com>
2024-12-08 18:42:03 +0200

    Update auth.js

--- src/services/discord/auth.js
+++ src/services/discord/auth.js
@@ -6,7 +6,7 @@ export const getDiscordToken = async (code) => {
     client_secret: import.meta.env.VITE_DISCORD_CLIENT_SECRET,
     grant_type: 'authorization_code',
     code: code,
-    redirect_uri: 'https://warm-sunburst-18c65c.netlify.app/auth/callback',
+    redirect_uri: 'https://private-web-xyz.vercel.app/auth/callback',
     scope: 'identify email'
   });
 
